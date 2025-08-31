947830009a5dcc6dd17773d91eb42f25c437b8da
공연정 <st3785@naver.com>
2025-08-31 21:16:11 +0900

    절대경로수정

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -13,11 +13,11 @@ with DAG( # DAG을 정의한는 부분
         
         t1_orange = BashOperator( # bash관련 기능이 담긴 객체 생성 -> 이걸 가지고 task 만듦
             task_id="t1_orange", # task명, 변수명과 동일해야 찾기 쉬움
-            bash_command="opt/airflow/plugins/shell/select_fruit.sh ORANGE")
+            bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE")
 
         t2_apple = BashOperator(
             task_id="t2_apple",
-            bash_command="opt/airflow/plugins/shell/select_fruit.sh APPLE"
+            bash_command="/opt/airflow/plugins/shell/select_fruit.sh APPLE"
         )
 
         t1_orange >> t2_apple
