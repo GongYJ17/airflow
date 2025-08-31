69d45ec823623a5bbb23c7caa6d08586e29cfe29
공연정 <st3785@naver.com>
2025-08-31 22:36:37 +0900

    python sys.path설정 후 테스트

--- dags/dags_python_import_fucn.py
+++ dags/dags_python_import_fucn.py
@@ -2,24 +2,21 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.providers.standard.operators.python import PythonOperator # (만든) python 함수를 실행시켜주는 역할
-import random
+# python 인터프리터는 plugins 부터 인식함. airflow는 sys.path에 plugins가 있어서 from common ~으로 작성해야함
+# 둘의 상충 문제를 해결하기 위해 .env를 사용
+from common.common_func import get_sftp
+
 
 with DAG( # DAG을 정의한는 부분
-    dag_id="dags_python_operator", # DAG python 파일명과 일치 권장
+    dag_id="dags_python_import_fucn", # DAG python 파일명과 일치 권장
     schedule="30 6 * * *", # 0시0분 (언제 도는지)
     start_date=pendulum.datetime(2025, 8, 31, tz="Asia/Seoul"), #UTC는 한국시간 +9임.
     catchup=False, # 소급적용을 할건지
 ) as dag:
 
-    def select_fruit():
-        fruit = ['apple', 'banana', 'cherry','avocado']
-        rand_int = random.randint(0, 3)
-        return fruit[rand_int]
-
-
-    py_t1 = PythonOperator(
+    task_get_sftp = PythonOperator(
         task_id="py_t1",
-        python_callable=select_fruit
+        python_callable=get_sftp
     )
 
-    py_t1
+    task_get_sftp
