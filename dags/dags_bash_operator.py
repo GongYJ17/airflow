a45e7587dbefaedd29b518852389cec90d3b967e
공연정 <st3785@naver.com>
2025-08-31 18:10:29 +0900

    yj수정3

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -9,7 +9,7 @@ with DAG( # DAG을 정의한는 부분
     schedule="0 0 * * *", # 0시0분 (언제 도는지)
     start_date=pendulum.datetime(2025, 8, 31, tz="Asia/Seoul"), #UTC는 한국시간 +9임.
     catchup=False, # 소급적용을 할건지
-    tags=["example", "example2", "example3"], # 해시태크들
+    #tags=["example", "example2", "example3"], # 해시태크들
 ) as dag:
     
     bash_t1 = BashOperator( # bash관련 기능이 담긴 객체 생성 -> 이걸 가지고 task 만듦
