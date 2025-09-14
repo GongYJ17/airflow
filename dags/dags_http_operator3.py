3a9b0694d8e59c097fb225257b479091d8c35593
공연정 <st3785@naver.com>
2025-09-14 22:38:37 +0900

    http 3 테스트

---
+++ dags/dags_http_operator3.py
@@ -0,0 +1,38 @@
+from airflow.sdk import DAG, task
+import pendulum
+import datetime
+from airflow.providers.http.operators.http import HttpOperator
+
+
+
+with DAG( # DAG을 정의한는 부분
+    dag_id="dags_http_operator3", # DAG python 파일명과 일치 권장
+    schedule=None, # 0시0분 (언제 도는지)
+    start_date=pendulum.datetime(2025, 8, 31, tz="Asia/Seoul"), #UTC는 한국시간 +9임.
+    catchup=False, # 소급적용을 할건지
+) as dag:
+    
+    get_diplomacy_info = HttpOperator(
+        task_id='get_diplomacy_info',
+        http_conn_id='openapi.data.go.kr',
+        endpoint='/1262000/DiplomacyJournalService',  # 실제 API 엔드포인트
+        method='GET',
+        data={
+            'serviceKey': 'EY9KD4N1rfHjBemZB5rmj/GdCrIwKdmHC2EibAahXYrY/2EW6glZA5kjIoLQ9lpMPSVNkcvqKzSg9tt5y8vhpA==',  # 여기에 실제 서비스 키 입력
+            'numOfRows': '10',
+            'pageNo': '1'
+        },
+        headers={"Content-Type": "application/json"},
+        extra_options={"verify": False}  # SSL 검증 비활성화
+    )
+
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='get_deplomacy_info')
+        import json
+        from pprint import pprint
+
+        pprint(json.loads(rslt))
+        
+    get_diplomacy_info >> python_2()
