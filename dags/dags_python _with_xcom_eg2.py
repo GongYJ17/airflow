837f0e0a02df0205ff0c938a592d648ae7f8ac3c
공연정 <st3785@naver.com>
2025-09-20 22:41:18 +0900

    xcom 실습2

---
+++ dags/dags_python _with_xcom_eg2.py
@@ -0,0 +1,31 @@
+from airflow.sdk import DAG, task
+
+with DAG(
+    dag_id='dags_python_with_xcom_eg2',
+    start_date=None,
+    schedule=None,
+    catchup=False
+) as dag:
+    @task(task_id = 'xcom_return_task')
+    def xcom_return(**kwargs):
+        return 'Success'
+
+    @task(task_id = 'python_xcom_pull_task')
+    def xcom_pull(**kwargs):
+        ti = kwargs['ti']
+        return_value = ti.xcom_pull(key='return_value', task_ids = 'xcom_return_task')
+        print(f'return_value: {return_value}')
+        only_task_ids = ti.xcom_pull(task_ids = 'xcom_return_task')
+        print(only_task_ids)
+    
+    @task(task_id = 'xcom_input_task')
+    def xcom_return_input(status, **kwargs):
+        print(status)
+
+    xcom_return_input(xcom_return())
+    xcom_return_task >> xcom_pull()
+
+
+
+
+    
