c365e9444972637311966ae52d5fa0119a60a516
공연정 <st3785@naver.com>
2025-09-20 23:06:27 +0900

    xcom 여러개 전달 실습

--- dags/dags_python_with_xcom_eg2.py
+++ dags/dags_python_with_xcom_eg2.py
@@ -8,7 +8,7 @@ with DAG(
 ) as dag:
     @task(task_id = 'xcom_return_task')
     def xcom_return(**kwargs):
-        return 'Success'
+        return 'Success' # {key1 : 'Suceess', key2 : 'Fail'} 로 여러개 전달가능.
 
     @task(task_id = 'python_xcom_pull_task')
     def xcom_pull(**kwargs):
@@ -21,11 +21,22 @@ with DAG(
     @task(task_id = 'xcom_input_task')
     def xcom_return_input(status, **kwargs):
         print(status)
+    
+    @task(task_id = 'xcom_return_multiple_task')
+    def xcom_multiple_return(**kwargs):
+        return {'key1' : 'Suceess', 'key2' : 'Fail'}
+
+    @task(task_id = 'xcom_input_multiple_task')
+    def xcom_multiple_return_input(status_dic, **kwargs):
+        print('status_dic['key1'] : ' + status_dic['key1'])
+        print('status_dic['key2'] : ' + status_dic['key2'])
 
 
-    xcom_return_task = xcom_return()
+
+     
+    xcom_return_task = xcom_return() #변수 선언 없이 하면 xcom_return()객체가 서로 다른 존재가 돼서 두개가 생겨버림. 
     xcom_return_task >> [xcom_pull(), xcom_return_input(xcom_return_task) ]
-
+    xcom_multiple_return_input(xcom_multiple_return())
 
 
 
