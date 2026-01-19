8726972a30e2e20905ad877b928f9630b124b642
공연정 <st3785@naver.com>
2026-01-19 14:30:36 +0900

    ts_op 수정

--- plugins/operators/collect_source_ts_operator.py
+++ plugins/operators/collect_source_ts_operator.py
@@ -5,7 +5,10 @@ class CollectSourceTsOperator(BaseOperator):
     """
     ts 전달
     """
+    def __init__(self, raw_base_path: str, **kwargs):
+        super().__init__(**kwargs)
+        self.raw_base_path = raw_base_path
 
     def execute(self, context):
-
+        self.log.info(f"[Transform] INPUT  : {self.raw_base_path}")
         return "20260119130000"   # ✅ XCom
