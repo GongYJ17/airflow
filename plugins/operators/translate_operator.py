a9d52381c4c3f3a6ba2cf0c36c67ffa3eab07628
공연정 <st3785@naver.com>
2026-01-20 15:52:05 +0900

    ts_nodash

--- plugins/operators/translate_operator.py
+++ plugins/operators/translate_operator.py
@@ -3,6 +3,7 @@ from airflow.models import BaseOperator
 from pathlib import Path
 import time
 from path.data_path_builder import DataPathBuilder
+from airflow.exceptions import AirflowException
 
 class TranslateOperator(BaseOperator):
     template_fields = ("ts")
@@ -13,12 +14,12 @@ class TranslateOperator(BaseOperator):
         self.ingest_type = ingest_type
 
     def execute(self, context):
-        if self.collection_nm == 'int_dplm_comm_general':
-            time.sleep(10)
 
         builder = DataPathBuilder(self.ingest_type)
 
         path = builder.staging(self.collection_nm, self.ts)
         self.log.info(f"[Translate] TARGET : {path}")
 
-
+        if self.collection_nm == 'int_dplm_comm_general':
+            time.sleep(10)
+            raise AirflowException("의도적으로 실패시킨 task")
