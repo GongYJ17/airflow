d987d8f08ded436ad764cd51c9e2e9dad30d4618
공연정 <st3785@naver.com>
2026-01-20 15:29:06 +0900

    tz 에러해결

--- dags/int_dplm_comm_pipeline_v6.py
+++ dags/int_dplm_comm_pipeline_v6.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.utils.task_group import TaskGroup
 from datetime import datetime
+import pendulum
 
 from operators.collect_source_ts_operator import CollectSourceTsOperator
 from operators.transform_operator import TransformOperator
@@ -8,12 +9,15 @@ from operators.translate_operator import TranslateOperator
 from operators.ml_operator import MLOperator
 
 
+KST = pendulum.timezone("Asia/Seoul")
+
 with DAG(
     dag_id="int_dplm_comm_pipeline_v6",
     start_date=datetime(2024, 1, 1),
     schedule=None,
     catchup=False,
     tags=["int_dplm_comm"],
+    timezone=KST
 ) as dag:
 
     ingest_type = "init"  # "init" | "incremental"
@@ -33,21 +37,21 @@ with DAG(
             transform = TransformOperator(
                 task_id=f"transform",
                 collection_nm=collection_nm,
-                ts="{{ dag_run.start_date | tz('Asia/Seoul') | strftime('%Y%m%d%H%M%S') }}",
+                ts="{{ dag_run.start_date.strftime('%Y%m%d%H%M%S') }}",
                 ingest_type=ingest_type,
             )
 
             translate = TranslateOperator(
                 task_id=f"translate",
                 collection_nm=collection_nm,
-                ts="{{ dag_run.start_date | tz('Asia/Seoul') | strftime('%Y%m%d%H%M%S') }}",
+                ts="{{ dag_run.start_date.strftime('%Y%m%d%H%M%S') }}",
                 ingest_type=ingest_type,
             )
 
             ml = MLOperator(
                 task_id=f"ml",
                 collection_nm=collection_nm,
-                ts="{{ dag_run.start_date | tz('Asia/Seoul') | strftime('%Y%m%d%H%M%S') }}",
+                ts="{{ dag_run.start_date.strftime('%Y%m%d%H%M%S') }}",
                 ingest_type=ingest_type,
             )
 
