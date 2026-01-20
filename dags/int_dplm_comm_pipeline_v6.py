a32906ab890c495689d28d9b6464863c1e382e07
공연정 <st3785@naver.com>
2026-01-20 16:00:09 +0900

    clear task

--- dags/int_dplm_comm_pipeline_v6.py
+++ dags/int_dplm_comm_pipeline_v6.py
@@ -34,21 +34,21 @@ with DAG(
             transform = TransformOperator(
                 task_id=f"transform",
                 collection_nm=collection_nm,
-                ts="{{ ts_nodash }}",
+                ts="{{ dag_run.start_date.strftime('%Y%m%d%H%M%S') }}",
                 ingest_type=ingest_type,
             )
 
             translate = TranslateOperator(
                 task_id=f"translate",
                 collection_nm=collection_nm,
-                ts="{{ ts_nodash }}",
+                ts="{{ dag_run.start_date.strftime('%Y%m%d%H%M%S') }}",
                 ingest_type=ingest_type,
             )
 
             ml = MLOperator(
                 task_id=f"ml",
                 collection_nm=collection_nm,
-                ts="{{ ts_nodash }}",
+                ts="{{ dag_run.start_date.strftime('%Y%m%d%H%M%S') }}",
                 ingest_type=ingest_type,
             )
 
